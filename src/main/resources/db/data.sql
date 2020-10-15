98220fe2d71ca11ca6fd384c8aaf7ec7bf9d1132
LipaMar <marcin.lipa@onet.pl>
2020-10-15 17:30:54 +0200

    datasource properties & initial schema+data

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,330 @@
+insert into students
+values (1, 'Kaylee', 'Careswell', 'kcareswell0@eventbrite.com', '53whrKbJX', 5, 'HLA', 51573, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (2, 'Benedicto', 'Hanks', 'bhanks1@altervista.org', '9zX30CK', 2, 'DDE', 52124, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (3, 'Bobette', 'Craker', 'bcraker2@desdev.cn', 'IomDYK', 5, 'Cellular Communications', 65430, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (4, 'Brady', 'Beaten', 'bbeaten3@google.es', 'psL5uu', 3, 'VB.NET', 15779, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (5, 'Lorine', 'Campbell-Dunlop', 'lcampbelldunlop4@wikimedia.org', '70ItYum9rN', 6, 'Offshore Drilling', 12281,
+        'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (6, 'Brook', 'Kingcott', 'bkingcott5@nbcnews.com', 'NIy1lpqizU', 3, 'Geology', 84140, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (7, 'Opal', 'Demaid', 'odemaid6@squarespace.com', 'fkJHvvXvSrZ', 3, 'Stage Make-up', 63318, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (8, 'Rochester', 'Vela', 'rvela7@tinyurl.com', 'g3CWepfoF', 3, 'Commercial Kitchen Design', 43704,
+        'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (9, 'Noami', 'Bodell', 'nbodell8@chron.com', 'waNNCURqxa36', 4, 'CFI', 46980, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (10, 'Keelia', 'Bauldry', 'kbauldry9@cornell.edu', 'oal8RDfl', 1, 'MX960', 79058, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (11, 'Christin', 'Few', 'cfewa@ebay.co.uk', 'PAnfu493U', 1, 'KT', 85913, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (12, 'Gasper', 'Rogeon', 'grogeonb@wordpress.org', 'Gci068Tw', 2, 'Autism Spectrum Disorders', 26242,
+        'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (13, 'Ruperta', 'Clibbery', 'rclibberyc@behance.net', 'RGqZnCIe21N', 7, 'BPT', 22800, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (14, 'Teddie', 'Menguy', 'tmenguyd@nba.com', 'UmllKaOBn', 4, 'RPL', 73955, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (15, 'Mozelle', 'Czaple', 'mczaplee@chron.com', 'CA3qTk2ydX0b', 3, 'Outlook Express', 89153, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (16, 'Jacinta', 'Amori', 'jamorif@washingtonpost.com', 'gjzVh8', 6, 'HCP', 36856, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (17, 'Nomi', 'Youde', 'nyoudeg@va.gov', 'zG5JT3Kh7', 3, 'Urban Regeneration', 50817, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (18, 'Dara', 'Tower', 'dtowerh@bbc.co.uk', 'pa2M5VuVno78', 4, 'Development Economics', 11069, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (19, 'Chrotoem', 'Eymer', 'ceymeri@bluehost.com', 'CbBXd4a2', 4, 'SAP-SD', 48170, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (20, 'Cassandre', 'Abdon', 'cabdonj@mac.com', 'pBQJ64zGt', 2, 'Planned Giving', 39832, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (21, 'Kipp', 'Delea', 'kdeleak@illinois.edu', 'FwNlYTZp', 3, 'CDS', 15774, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (22, 'Kirsteni', 'Grayley', 'kgrayleyl@google.co.uk', 'GVXcbkWMmliY', 2, 'Business Ideas', 83188, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (23, 'Alexis', 'Leathlay', 'aleathlaym@php.net', 'GQOJE3uJ', 4, 'TSRM', 77788, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (24, 'Homere', 'Hafford', 'hhaffordn@reuters.com', 'k7OdoX', 2, 'Wholesale', 99975, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (25, 'Wang', 'Langcaster', 'wlangcastero@rambler.ru', 'kDfGRxmNfJwh', 7, 'XML-RPC', 63249, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (26, 'Dale', 'Flewitt', 'dflewittp@state.gov', 'WwTkyB', 1, 'Nastran', 42427, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (27, 'Geneva', 'Kulic', 'gkulicq@altervista.org', '0XXW8dh', 3, 'Permanent Placement', 71343, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (28, 'Lucila', 'Yurlov', 'lyurlovr@lulu.com', 'vbaC2aoZ5', 5, 'Offshore Oil &amp; Gas', 65483, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (29, 'Yetty', 'Gontier', 'ygontiers@un.org', 'HQ4E0GwJ', 2, 'Customer Retention', 48398, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (30, 'Lenette', 'Grinvalds', 'lgrinvaldst@skype.com', 'dLdUQe', 3, 'Sales Operations', 28912, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (31, 'Annadiane', 'Domeny', 'adomenyu@ustream.tv', 'JJyA4vMz', 3, 'NDM', 94557, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (32, 'Sanders', 'Haruard', 'sharuardv@newsvine.com', 'b6t1jiHDu', 3, 'Agribusiness', 45208, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (33, 'Pollyanna', 'Niblett', 'pniblettw@etsy.com', '8bjvDEmA6gP', 4, 'Print On Demand', 24821, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (34, 'Petronille', 'Abelov', 'pabelovx@ocn.ne.jp', 'SUXpaRCEvzP', 6, 'European Union Politics', 41810,
+        'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (35, 'Dorri', 'Yeates', 'dyeatesy@geocities.com', 'sNSYJ7wxJ8zj', 5, 'XFP', 80804, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (36, 'Torr', 'Burgyn', 'tburgynz@jugem.jp', 'RXt9OBSIZv', 6, 'Photoshop', 62602, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (37, 'Brendon', 'Origan', 'borigan10@tmall.com', '4qZAvM7Dli', 7, 'Sports Play-by-play', 57059, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (38, 'Nanny', 'Goulborn', 'ngoulborn11@fema.gov', 'Gqd7apN1XxKW', 2, 'Water Treatment', 10320, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (39, 'Rebekah', 'Coppen', 'rcoppen12@youku.com', 'jLHr8OkqQ', 4, 'Genetics', 65734, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (40, 'Leoine', 'Nials', 'lnials13@google.es', '8pAhTtJ', 1, 'JSF', 37161, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (41, 'Kath', 'Bute', 'kbute14@sourceforge.net', 'acSZLxvGe', 7, 'CFRA', 88053, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (42, 'Leila', 'Krale', 'lkrale15@blogspot.com', 'MpRpoy1Ktgj', 2, 'DFX', 67571, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (43, 'Ealasaid', 'Game', 'egame16@lycos.com', 'qSvrEZB', 5, 'PDA', 57044, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (44, 'Cristie', 'Davidovics', 'cdavidovics17@wsj.com', 'CDjBI8dZc', 7, 'Gcov', 82740, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (45, 'Douglass', 'Dudbridge', 'ddudbridge18@360.cn', '2ffIHvJDzh', 3, 'Jib', 54495, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (46, 'Michal', 'Sturdy', 'msturdy19@arstechnica.com', 'efN6Uro3NDK', 3, 'Hazardous Materials', 78634,
+        'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (47, 'Zechariah', 'Tegeller', 'ztegeller1a@purevolume.com', 'FsAaDKGPnZm', 5, 'CSLA', 84939, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (48, 'Yevette', 'Laurenson', 'ylaurenson1b@alibaba.com', '0gAb4YbqOj', 4, 'Drywall', 26216, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (49, 'Chantal', 'Gallant', 'cgallant1c@google.ru', 'joZQwEwvQ', 3, 'Naval Architecture', 57512, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+insert into students
+values (50, 'Rafaello', 'Coat', 'rcoat1d@adobe.com', 'K0DKTgxna', 6, 'Swing', 31654, 'ROLE_STUDENT')
+ON CONFLICT DO NOTHING;
+
+insert into meetings
+values (1, 'quam turpis adipiscing lorem vitae mattis',
+        'dolor quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae nisi',
+        'dolor vel est donec odio justo sollicitudin ut suscipit a feugiat et eros vestibulum ac est lacinia',
+        '2021-09-24 20:04:25')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (2, 'nibh ligula nec sem duis aliquam', 'fermentum justo',
+        'hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur',
+        '2021-02-13 00:08:10')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (3, 'orci pede venenatis non sodales sed',
+        'sodales sed tincidunt eu felis fusce posuere felis sed lacus morbi sem mauris laoreet ut rhoncus aliquet pulvinar sed',
+        'sit amet erat nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla',
+        '2021-03-20 22:22:01')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (4, 'magna ac consequat metus sapien ut nunc',
+        'accumsan odio curabitur convallis duis consequat dui nec nisi volutpat eleifend donec ut dolor morbi vel lectus in quam fringilla',
+        'lorem id ligula suspendisse ornare consequat lectus in est risus auctor sed', '2020-10-26 17:56:28')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (5, 'sed nisl nunc rhoncus dui', 'vel accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit',
+        'in sagittis dui vel nisl duis ac nibh fusce lacus', '2021-04-27 17:07:01')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (6, 'vestibulum eget',
+        'id ligula suspendisse ornare consequat lectus in est risus auctor sed tristique in tempus sit amet sem',
+        'tellus nulla ut erat id mauris vulputate elementum nullam varius nulla facilisi cras non',
+        '2021-07-06 14:35:50')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (7, 'nam congue risus semper porta volutpat quam',
+        'volutpat sapien arcu sed augue aliquam erat volutpat in congue etiam justo etiam',
+        'velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget',
+        '2021-03-12 01:58:30')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (8, 'turpis a pede posuere nonummy integer non',
+        'erat fermentum justo nec condimentum neque sapien placerat ante',
+        'non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla',
+        '2021-08-27 07:10:06')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (9, 'aliquam augue',
+        'eget tincidunt eget tempus vel pede morbi porttitor lorem id ligula suspendisse ornare consequat lectus in est risus auctor sed',
+        'hendrerit at vulputate vitae nisl aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula',
+        '2020-11-01 15:01:00')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (10, 'et commodo vulputate justo',
+        'morbi vestibulum velit id pretium iaculis diam erat fermentum justo nec condimentum neque sapien placerat ante nulla justo',
+        'lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum sed magna at nunc commodo placerat praesent blandit',
+        '2021-05-20 20:37:12')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (11, 'in felis eu sapien', 'laoreet ut rhoncus aliquet pulvinar sed nisl nunc rhoncus dui vel sem',
+        'donec pharetra magna vestibulum aliquet ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non mi integer ac',
+        '2020-12-06 08:18:40')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (12, 'donec ut dolor morbi vel lectus', 'convallis',
+        'pellentesque eget nunc donec quis orci eget orci vehicula condimentum curabitur in libero',
+        '2021-04-14 03:15:41')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (13, 'ultrices posuere cubilia curae nulla dapibus dolor',
+        'elementum nullam varius nulla facilisi cras non velit nec nisi vulputate nonummy maecenas',
+        'odio curabitur convallis duis consequat dui nec nisi volutpat eleifend donec ut dolor morbi vel lectus',
+        '2020-12-22 17:49:49')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (14, 'blandit mi in porttitor pede justo eu',
+        'varius nulla facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt',
+        'sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices',
+        '2020-11-04 21:51:28')
+ON CONFLICT DO NOTHING;
+insert into meetings
+values (15, 'ipsum primis in', 'neque duis bibendum morbi non quam nec dui luctus rutrum nulla tellus in sagittis',
+        'non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac habitasse platea dictumst',
+        '2021-04-04 17:25:08')
+ON CONFLICT DO NOTHING;
+
+insert into students_meetings (m_id, s_id)
+values (14, 10)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (8, 7)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (2, 29)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (5, 27)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (3, 1)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (3, 47)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (6, 44)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (2, 3)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (4, 49)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (13, 6)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (8, 1)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (13, 47)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (3, 4)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (8, 33)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (8, 28)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (8, 20)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (5, 15)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (6, 38)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (12, 7)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (1, 25)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (14, 37)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (5, 1)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (9, 43)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (11, 5)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (11, 15)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (3, 47)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (10, 16)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (6, 15)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (14, 7)
+ON CONFLICT DO NOTHING;
+insert into students_meetings (m_id, s_id)
+values (8, 41)
+ON CONFLICT DO NOTHING;
